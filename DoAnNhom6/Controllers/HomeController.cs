ea8557716597b2f753cceb6da172569b7b164ccd
TaiAnh88 <tranthienbao050@gmail.com>
2024-11-29 22:27:54 +0700

    them admin

--- DoAnNhom6/Controllers/HomeController.cs
+++ DoAnNhom6/Controllers/HomeController.cs
@@ -7,24 +7,18 @@ namespace DoAnNhom6.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly DoAnNhom6Context _context;
+		
 		private readonly ILogger<HomeController> _logger;
 
-<<<<<<< HEAD
-		public HomeController(DoAnNhom6Context context, ILogger<HomeController> logger)
-=======
-        public HomeController(ILogger<HomeController> logger)
->>>>>>> dba0d90f3e66eb184d996336070966e8702da3c3
-		{
-			_context = context;
-			_logger = logger;
-		}
 
+        public HomeController(ILogger<HomeController> logger)
+        {
+            Console.WriteLine("Remote changes"); // Thay ??i t? remote
+            
+            _logger = logger;
+        }
         public IActionResult Index()
 		{
-			ViewBag.productCategories = _context.TblProductCategories.ToList();
-			ViewBag.productNew = _context.TblProducts.Where(m=>m.IsNew).ToList();
-            var products = _context.TblProducts.ToList();
 
             //return View(products); // Truy?n danh sách s?n ph?m vào Model
 
--- DoAnNhom6/ViewComponents/ProductViewComponent.cs
+++ DoAnNhom6/ViewComponents/ProductViewComponent.cs
@@ -1,23 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using DoAnNhom6.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DoAnNhom6.ViewComponents
 {
-    public class ProductViewComponent: ViewComponent
+    public class ProductViewcomponent : ViewComponent
     {
         private readonly DoAnNhom6Context _context;
-        public ProductViewComponent(DoAnNhom6Context context)
+        public ProductViewcomponent(DoAnNhom6Context context)
         {
             _context = context;
         }
-        public async Task<IViewComponentResult> InvokeAsync() 
+
+        public async Task<IViewComponentResult> InvokeAsync(int categoryid)
         {
-            var items = await _context.TblProducts
+            var item = await _context.TblProducts
                 .Include(m => m.Category)
-                .Where(m => (bool)m.IsActive)
+                .Where(m => m.CategoryId  == categoryid)
+                //.Where(m => m.IsActive)
+                //.Where (m => m.IsNew)
                 .ToListAsync();
-            return View(items);
+
+            return View(item);
+
         }
     }
 }
