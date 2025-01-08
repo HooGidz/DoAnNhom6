692a4b229ecc898cb33a3e3e9a397ba072dfae33
TaiAnh88 <tranthienbao050@gmail.com>
2025-01-08 14:24:04 +0700

    chi tiết sản phẩm

--- DoAnNhom6/Controllers/ContactController.cs
+++ DoAnNhom6/Controllers/ContactController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DoAnNhom6.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DoAnNhom6.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly DoAnNhom6Context _context;
+        public ContactController(DoAnNhom6Context Context)
+        {
+            _context = Context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(string name, string email, string phone, string message)
+        {
+            try
+            {
+                TblContact contact = new TblContact();
+                contact.Name = name;
+                contact.Phone = phone;
+                contact.Email = email;                
+                contact.Message = message;
+                _context.TblContacts.Add(contact);
+                _context.SaveChanges();
+                return Json(new { startus = true });
+            }
+            catch
+            {
+                return Json(new { startus = false });
+            }
         }
     }
 }
--- DoAnNhom6/Controllers/ProductController.cs
+++ DoAnNhom6/Controllers/ProductController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DoAnNhom6.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DoAnNhom6.Controllers
 {
     public class ProductController : Controller
     {
+
+        private readonly DoAnNhom6Context _context;
+
+        public ProductController(DoAnNhom6Context context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [Route("/product/{alias}-{id}.html")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TblProducts == null)
+            {
+                return NotFound();
+            }
+            var product = await _context.TblProducts.Include(i => i.Category)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewBag.productReView = _context.TblProductReviews
+                .Where(i => i.ProductId == id).ToList();
+            ViewBag.productRelated = _context.TblProducts.
+                Where(i => i.ProductId != id && i.CategoryId == product.CategoryId).Take(5).
+                OrderByDescending(i => i.ProductId).ToList();
+            return View(product);
         }
     }
 }
