a025f930d7ed9c64bce4c3039bed9fb2aa45f5e8
HooGidz <hoangdinhgiap04@gmail.com>
2025-01-08 15:43:36 +0700

    Gui lien he

--- DoAnNhom6/Controllers/ContactController.cs
+++ DoAnNhom6/Controllers/ContactController.cs
@@ -1,37 +1,40 @@
-﻿using DoAnNhom6.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using DoAnNhom6.Models;
 
-namespace DoAnNhom6.Controllers
+namespace Harmic.Controllers
 {
     public class ContactController : Controller
     {
         private readonly DoAnNhom6Context _context;
-        public ContactController(DoAnNhom6Context Context)
+        public ContactController(DoAnNhom6Context context)
         {
-            _context = Context;
+            _context = context;
         }
         public IActionResult Index()
         {
             return View();
         }
-        [HttpPost]
-        public IActionResult Create(string name, string email, string phone, string message)
+        [HttpPost("/contact/SendContact")]
+        public async Task<IActionResult> SendContact([Bind("Name,Phone,Email,Message")] TblContact contact)
         {
-            try
+            if (ModelState.IsValid)
             {
-                TblContact contact = new TblContact();
-                contact.Name = name;
-                contact.Phone = phone;
-                contact.Email = email;                
-                contact.Message = message;
-                _context.TblContacts.Add(contact);
-                _context.SaveChanges();
-                return Json(new { startus = true });
+                contact.IsRead = 0;
+                contact.CreatedDate = DateTime.Now;
+                contact.CreatedBy = contact.Name;
+
+                _context.Add(contact);
+                await _context.SaveChangesAsync();
+                TempData["StatusMessage"] = "Gửi liên hệ thành công!";
+                return RedirectToAction("Index"); // Điều hướng về trang Index
             }
-            catch
-            {
-                return Json(new { startus = false });
-            }
+            TempData["StatusMessage"] = "Đã có lỗi xảy ra. Vui lòng kiểm tra lại thông tin.";
+            return RedirectToAction("Index");
         }
+
+
     }
 }
