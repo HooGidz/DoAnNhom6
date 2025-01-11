7c50ced553e8c91877db6ed5e46b662ffe0aeb0c
ViThinh <130222331+ViTheThinh@users.noreply.github.com>
2025-01-11 18:19:50 +0700

    Update Function.cs

--- DoAnNhom6/UtilitiesHelper/Function.cs
+++ DoAnNhom6/UtilitiesHelper/Function.cs
@@ -6,5 +6,35 @@
         {
             return SlugGenerator.SlugGenerator.GenerateSlug(title);
         }
+        public static int _UserId = 0;
+        public static string _Username = String.Empty;
+        public static string _Email = String.Empty;
+        public static string _Message = string.Empty;
+        public static string _MessageEmail = string.Empty;
+        public static string MD5Hash(string text)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+            byte[] result = md5.Hash;
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++) 
+            { 
+            strBuilder.Append(result[i].ToString("x2"));             
+            }
+            return strBuilder.ToString();
+        }
+        public static string MD5Password(string? text)
+        {
+            string str = MD5Hash(text);
+            for (int i = 0; i <= 5;  i++) 
+                str = MD5Hash(str + "-" + str);
+            return str;
+        }
+        public static bool IsLogin()
+        {
+            if (string.IsNullOrEmpty(Functions._Username) || string.IsNullOrEmpty(Functions._Email) || (Functions._UserId <= 0))
+                return false;
+            return true; 
+        }
     }
 }
