9e16a52e7ef4dd574cf1300862d3d59a2abb9d24
ViThinh <130222331+ViTheThinh@users.noreply.github.com>
2025-01-12 00:27:31 +0700

    Update RegisterController.cs

--- DoAnNhom6/Areas/Admin/Controllers/Register/RegisterController.cs
+++ DoAnNhom6/Areas/Admin/Controllers/Register/RegisterController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using DoAnNhom6.Areas.Admin.Models;
+using DoAnNhom6.Models;
+using DoAnNhom6.Utilities;
 
-namespace DoAnNhom6.Controllers.Register
+namespace DoAnNhom6.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class RegisterController : Controller
@@ -18,17 +21,25 @@ namespace DoAnNhom6.Controllers.Register
             return View();
         }
         [HttpPost]
-        public IActionResult Index(User user)
+        public IActionResult Index(TblUser user)
         {
             if (user == null)
             {
                 return View();
             }
-            var check = _context.User.Where(m => m.Email == user.Email).FirsOrDefault();
+            var check = _context.TblUser.FirstOrDefault(m => m.Email == user.Email);
             if (check != null)
             {
-                Functions._MassageEmail = "Duplicate Email!";
+                Functions._MessageEmail = "Duplicate Email!";
                 return RedirectToAction("Index", "Register");
             }
+            Functions._MessageEmail = string.Empty;
+            user.Password = Functions.SHA256Hash(user.Password); 
+            _context.TblUser.Add(user);
+            _context.SaveChanges(); 
+
+            return RedirectToAction("Success", "Register"); 
         }
     }
+}
+
