3d7c9ef5fb0c218ec1e2ce3c3ef2d03fa620deea
ViThinh <130222331+ViTheThinh@users.noreply.github.com>
2025-01-12 00:22:06 +0700

    Update Function.cs

--- DoAnNhom6/UtilitiesHelper/Function.cs
+++ DoAnNhom6/UtilitiesHelper/Function.cs
@@ -11,17 +11,18 @@
         public static string _Email = String.Empty;
         public static string _Message = string.Empty;
         public static string _MessageEmail = string.Empty;
-        public static string MD5Hash(string text)
+        public static string SHA256Hash(string text)
         {
-            MD5 md5 = new MD5CryptoServiceProvider();
-            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
-            byte[] result = md5.Hash;
-            StringBuilder strBuilder = new StringBuilder();
-            for (int i = 0; i < result.Length; i++) 
-            { 
-            strBuilder.Append(result[i].ToString("x2"));             
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] result = sha256.ComputeHash(Encoding.ASCII.GetBytes(text));
+                StringBuilder strBuilder = new StringBuilder();
+                for (int i = 0; i < result.Length; i++)
+                {
+                    strBuilder.Append(result[i].ToString("x2"));
+                }
+                return strBuilder.ToString();
             }
-            return strBuilder.ToString();
         }
         public static string MD5Password(string? text)
         {
