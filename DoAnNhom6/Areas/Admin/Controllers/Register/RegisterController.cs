b46b3fd6b962c91b13020bc002ff40f4eee18153
ViThinh <130222331+ViTheThinh@users.noreply.github.com>
2025-01-11 18:14:31 +0700

    Add files via upload

---
+++ DoAnNhom6/Areas/Admin/Controllers/Register/RegisterController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace DoAnNhom6.Controllers.Register
+{
+    [Area("Admin")]
+    public class RegisterController : Controller
+    {
+        private readonly DbContext _context;
+        public RegisterController(DbContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            if (user == null)
+            {
+                return View();
+            }
+            var check = _context.User.Where(m => m.Email == user.Email).FirsOrDefault();
+            if (check != null)
+            {
+                Functions._MassageEmail = "Duplicate Email!";
+                return RedirectToAction("Index", "Register");
+            }
+        }
+    }
