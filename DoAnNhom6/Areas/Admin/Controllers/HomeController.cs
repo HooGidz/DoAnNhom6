ddc31b50a6b242456ca12f63c172a4c6fc49e3e3
HooGidz <hoangdinhgiap04@gmail.com>
2025-01-12 19:29:44 +0700

    Dang Xuat Admin

--- DoAnNhom6/Areas/Admin/Controllers/HomeController.cs
+++ DoAnNhom6/Areas/Admin/Controllers/HomeController.cs
@@ -14,5 +14,14 @@ namespace DoAnNhom6.Areas.Admin.Controllers
             }
             return View();
         }
+        public IActionResult Logout()
+        {
+            DoAnNhom6.Utilities.Function._UserId = 0;
+            DoAnNhom6.Utilities.Function._Username = string. Empty;
+            DoAnNhom6.Utilities.Function._Email = string. Empty;
+            DoAnNhom6.Utilities.Function._Message = string. Empty;
+            DoAnNhom6.Utilities.Function._MessageEmail = string. Empty;
+            return RedirectToAction("Index","Login");
+        }
     }
 }
